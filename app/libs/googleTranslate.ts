bb3e7430744f7e6c2091c30f8d0a887d1908b07a
Egbadon Victor <thethinkerx100@gmail.com>
2023-06-14 20:10:14 +0100

    fixed security issue

--- app/libs/googleTranslate.ts
+++ app/libs/googleTranslate.ts
@@ -3,7 +3,6 @@ const { Translate } = require("@google-cloud/translate").v2;
 const projectId = process.env.GOOGLE_PROJECT_ID;
 const keyFilename = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
-const translateClient = new Translate({ projectId, keyFilename });
-
+const translateClient = new Translate({ projectId });
 
 export default translateClient;
