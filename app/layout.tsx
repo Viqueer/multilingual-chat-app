e81f3ca67ca4e3de572211e3595b41e7e887aa6b
Egbadon Victor <thethinkerx100@gmail.com>
2023-06-15 06:38:03 +0100

    change: changed the name of the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import ActiveStatus from "./components/ActiveStatus";
 import ToasterContext from "./context/ToasterContext";
 
 export const metadata = {
-  title: "Messenger",
-  description: "Messenger Clone",
+  title: "Multilingua",
+  description: "A multilingual chat app",
 };
 
 export default async function RootLayout({
